ef2e8ed8c4b39b3e3e46b66b7f48f351b5eed5aa
AMT101 <amitanshupattanayak99@gmail.com>
2019-06-29 19:55:52 +0530

    added keys file

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+//add this file to gitignore
+
+module.exports = {
+	google:{
+		clientID: "341403928407-hpsfi69jajs064o303brl9jqnogaj3p8.apps.googleusercontent.com",
+		clientSecret: "8P44zwvurhL_n7Pv6vrw1eSO"
+	},
+
+	mongoDB:{
+		dbURI: "mongodb+srv://amt101:Grant1access!@cluster0-3gcff.mongodb.net/test?retryWrites=true&w=majority"
+	},
+
+	session:{
+		cookieKey: "thiskeyismineandonlymine"
+	}
+};
