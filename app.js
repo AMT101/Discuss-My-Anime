f2475a6d0bf529e393cc355daef3bf87ffdbc30c
AMT101 <amitanshupattanayak99@gmail.com>
2019-06-30 23:26:44 +0530

    restored the previous changes

--- app.js
+++ app.js
@@ -38,20 +38,6 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.use(function(req, res, next) {
-        if (process.env.NODE_ENV === "production") {
-            const reqType = req.headers["x-forwarded-proto"];
-            // if not https redirect to https unless logging in using OAuth
-            if (reqType !== "https") {
-                req.url.indexOf("auth/google") !== -1
-                  ? next()
-                  : res.redirect("https://" + req.headers.host + req.url);
-            }
-        } else {
-            next();
-        }
-    }); 
-
 //requiring routes
 const seriesRoutes = require("./routes/series");
 const chatsRoutes = require("./routes/chats");
